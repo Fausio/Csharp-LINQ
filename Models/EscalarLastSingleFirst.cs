2b937368a3b30de6576a8c7d5b2699b7f21c1dff
fausio luis <luisfausio@gmail.com>
2020-11-29 08:45:50 +0200

    Escalar Last Single First

---
+++ Models/EscalarLastSingleFirst.cs
@@ -0,0 +1,28 @@
+
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQ.Models
+{
+    public class EscalarLastSingleFirst
+    {
+        public int[] empty = Enumerable.Empty<int>().ToArray();
+        public int[] numbers = Enumerable.Range(0, 10).ToArray();
+        public static[] Names = { "Fausio", "Luis", "Matsinhe", "João", "Ilda" };
+
+        public int GetFist() =>
+        numbers.FirstOrDefault(); // 0
+
+        public int GetSingle() =>
+        numbers.SingleOrDefault(s => s / 2 == 1); // 2
+
+        public int GetLast() =>
+        numbers.Last(last => last == 2); // 2
+
+        public int GetElemetByIndex() =>
+        Enumerable.Range(0, 5).ElementAt(3); // 2
+
+    }
+}
