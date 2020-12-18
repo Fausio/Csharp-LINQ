eebce9de078802e46acddc3e3d78085714b9d21a
fausio luis <luisfausio@gmail.com>
2020-12-18 07:44:58 +0200

    EnumManipulator (JoinEnum, Write)

---
+++ Models/EnumManipulator.cs
@@ -0,0 +1,14 @@
+using System;
+using LINQ.Models;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQ.Models
+{
+    public class EnumManipulator
+    {
+        public string JoinEnum<T>(IEnumerable<T> items) => string.Join(", ",items);
+        public void Write<T>(IEnumerable<T> ToWrite) => Console.WriteLine(JoinEnum(ToWrite));
+    }
+}
