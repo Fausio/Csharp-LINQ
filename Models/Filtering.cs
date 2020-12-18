9e5bf05344be689d30f83aafdf09b7315e810ea2
fausio luis <luisfausio@gmail.com>
2020-12-18 07:13:44 +0200

    populateDate

--- Models/Filtering.cs
+++ Models/Filtering.cs
@@ -1,7 +1,21 @@
+using System;
+using LINQ.Models;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace LINQ.Models
 {
     public class Filtering
     {
-        public ObjectClass date = new ObjectClass();
+        public List<ObjectClass> MyData = new List<ObjectClass>();
+
+        public void populateDate()
+        {
+            foreach (int item in Enumerable.Range(0,10))
+            {
+              MyData.Add(new ObjectClass(){ Number = item});
+            }
+        }
     }
 }
