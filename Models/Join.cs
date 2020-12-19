ac792584504418e78acbc26aecf4c27661302154
fausio luis <luisfausio@gmail.com>
2020-12-19 08:01:04 +0200

    JoinMethot

--- Models/Join.cs
+++ Models/Join.cs
@@ -8,6 +8,18 @@ namespace LINQ.Models
 {
     public class Join
     {
+        public IEnumerable<int> ListOne = Enumerable.Range(0, 11);// 0..10
+        public IEnumerable<int> LIstTwo = Enumerable.Range(5, 6); // 5 - 10
+
+        public void JoinMethot()
+        {
+
+            IEnumerable<int> query = from i in ListOne
+                                     join jj in LIstTwo on i equals jj
+                                     select i;
+
+            Console.WriteLine(string.Join(" ,", query));
+        }
 
     }
 }
